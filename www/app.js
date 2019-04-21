2bce2cd58bc3c350a596e9c8ff26dc218f070a00
nakanishihome <s_nakanishi@app-crusaders.com>
2019-04-21 22:29:36 +0900

    modify

--- www/app.js
+++ www/app.js
@@ -20,3 +20,24 @@ var app = new Framework7({
 });
 
 var mainView = app.views.create('.view-main');
+
+var aaa = "<div>あああ</div>";
+aaa = Template7.helpers.escape(aaa);
+console.log(aaa);
+
+var $$ = Dom7;
+var bbb = $$('#template').html();
+ 
+// compile it with Template7
+var compiledTemplate = Template7.compile(bbb);
+
+var last = '<div>中西</div>';
+var first = '<div>真二</div>';
+ 
+// Now we may render our compiled template by passing required context
+var context = {
+    firstName: last,
+    lastName: first
+};
+var html = compiledTemplate(context);
+console.log(html);
